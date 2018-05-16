4a9b13207c65b22e7861836a1c57f58cd6bfc059
sungjk <ajax0615@gmail.com>
2018-05-16 23:23:20 +0900

    Initial commit

--- src/test/scala/com/sungjk/salgorithm/common/GlobalGraphTest.scala
+++ src/test/scala/com/sungjk/salgorithm/common/GlobalGraphTest.scala
@@ -2,6 +2,6 @@ package com.sungjk.salgorithm.common
 
 import org.scalatest.{Matchers, WordSpecLike}
 
-class GlobalGraphTest extends WordSpecLike with Matchers
+class GlobalGraphTest extends WordSpecLike with Matchers {
 
 }
